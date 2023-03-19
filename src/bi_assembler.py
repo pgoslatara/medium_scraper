b2606d7d6d5ea69200db825b98e1f11e92fdbb6f
pgoslatara <pgoslatara@gmail.com>
2023-03-19 19:10:17 +0100

    Updating plotly to subplots

--- src/bi_assembler.py
+++ src/bi_assembler.py
@@ -2,6 +2,7 @@ import duckdb
 import os
 from pathlib import Path
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from utils.utils import *
 
 
@@ -23,13 +24,24 @@ class BiAssembler:
             )
 
     def run(self):
+        # Use duckdb to read mart from data lake
         df = self.get_duckdb_con().execute(f"""
             SELECT
                 *
             FROM read_parquet('{os.getenv('DATA_DIR')}/marts/fct_blogs_per_day.parquet')
         """).df()
-        fig = go.Figure([go.Bar(x=list(df.published_date), y=list(df.num_blogs))])
 
+        # Create subplot per tag
+        tags = list(sorted(df['tag'].unique().tolist()))
+        fig = make_subplots(rows=len(tags), cols=1, subplot_titles=tags)
+        for tag in tags:
+            fig.append_trace(go.Bar(
+                x=list(df[df.tag==tag].published_date), y=list(df[df.tag==tag].num_blogs), showlegend=False
+            ), row=tags.index(tag)+1, col=1)
+
+        fig.update_layout(title_text="Medium Blog posts per tag per day")
+        
+        # Save to html file in data lake
         bi_html_filename = f"{os.getenv('DATA_DIR')}/bi/index.html"
         Path(bi_html_filename[:bi_html_filename.rfind("/")]).mkdir(parents=True, exist_ok=True)
         logging.info(f"Saving plotly html to {bi_html_filename}...")
