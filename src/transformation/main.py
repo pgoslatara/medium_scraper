4e413d320e72722989d60146f7952748ccf2825c
pgoslatara <pgoslatara@gmail.com>
2023-05-11 21:05:18 +0200

    Workflow deps (#45)
    * Moving automerge step
    
    * Dedicated github pages workflow
    
    * Adding GitHub CLI login step
    
    * Restoring sh to run dbt commands
    
    * Correcting gh login command
    
    * Installing gh in runner
    
    * Using sudo for install
    
    * Clearing env var
    
    * Adding debug steps
    
    * Ensuring personal PAT used to create PR
    
    * Tidying up step
    
    * Adding squash arg

--- src/transformation/main.py
+++ src/transformation/main.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-from utils.utils import run_dbt_command
+from utils.utils import run_dbt_commands
 
 
 def main() -> None:
@@ -9,9 +9,7 @@ def main() -> None:
     Path(f"{os.getenv('DATA_DIR')}/marts/").mkdir(parents=True, exist_ok=True)
 
     # Run dbt to update marts
-    if "dbt_packages" not in [f.name for f in Path(".").iterdir() if f.is_dir()]:
-        run_dbt_command("dbt deps")
-    run_dbt_command("dbt build")
+    run_dbt_commands(["dbt deps", "dbt build"])
 
 
 if __name__ == "__main__":
