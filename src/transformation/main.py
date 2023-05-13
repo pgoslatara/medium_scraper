71c088a9a6f76f9b013be9768ebc56dd9a0a7864
pgoslatara <pgoslatara@gmail.com>
2023-05-13 17:26:20 +0200

    Updating dbt DAG

--- src/transformation/main.py
+++ src/transformation/main.py
@@ -10,10 +10,14 @@ def main() -> None:
     # Ensure required directories exist
     Path(f"{os.getenv('DATA_DIR')}/marts/").mkdir(parents=True, exist_ok=True)
 
-    # Run dbt to update marts
-    run_dbt_commands(["dbt deps", "dbt build"])
+    run_dbt_commands(["dbt deps"])
 
+    # nlp_main depends on a dbt model
+    run_dbt_commands(["dbt build --select dim_medium_authors"])
     nlp_main()
+
+    # Run dbt to update marts
+    run_dbt_commands(["dbt build"])
 
 
 if __name__ == "__main__":
