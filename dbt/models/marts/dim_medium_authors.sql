a6a834d85452345c5f2d51d97e06c26a69c2d169
pgoslatara <pgoslatara@gmail.com>
2023-05-13 17:57:32 +0200

    Simpler dbt model

--- dbt/models/marts/dim_medium_authors.sql
+++ dbt/models/marts/dim_medium_authors.sql
@@ -4,13 +4,5 @@
     )
 }}
 
-select
-    a.author_name,
-    a.author_url,
-    b.author_name,
-    a.num_followers,
-    a.short_bio,
-    a.extracted_at
-from {{ ref('stg_medium_authors__authors') }} a
-left join {{ ref('stg_medium_blogs__articles') }} b on b.author_url = a.author_url
-group by 1, 2, 3, 4, 5, 6
+select author_name, author_url, author_name, num_followers, short_bio, extracted_at
+from {{ ref('stg_medium_authors__authors') }}
