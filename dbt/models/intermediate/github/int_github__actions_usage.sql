be7f10232341aa0f557973f7d9e6e5fade76dfa9
pgoslatara <pgoslatara@gmail.com>
2023-05-19 09:53:41 +0200

    Correcting GitHub billable minutes logic

--- dbt/models/intermediate/github/int_github__actions_usage.sql
+++ dbt/models/intermediate/github/int_github__actions_usage.sql
@@ -2,11 +2,20 @@ with
     usage as (
         select
             workflow_run_id,
-            sum(date_diff('seconds', started_at, completed_at)) as usage_sec
+            job_id,
+            date_diff('seconds', started_at, completed_at) as usage_sec,
+            ceiling(cast(coalesce(usage_sec, 0) as numeric) / 60) as billable_minutes  -- billable minutes are calculated per job per minute
         from {{ ref('stg_github_actions__jobs') }}
+    ),
+    agg_usage as (
+        select
+            workflow_run_id,
+            sum(usage_sec) as usage_sec,
+            sum(billable_minutes) as billable_minutes
+        from usage
         group by 1
-        order by 1
     )
-select w.*, coalesce(usage.usage_sec, 0) as usage_sec
+
+select w.*, agg_usage.usage_sec, agg_usage.billable_minutes
 from {{ ref('stg_github_actions__workflow_runs') }} w
-left join usage on usage.workflow_run_id = w.workflow_run_id
+left join agg_usage on agg_usage.workflow_run_id = w.workflow_run_id
