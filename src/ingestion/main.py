fc161c2ac443bf8a3057383c2c46541601e024f0
Pádraic Slattery <pgoslatara@gmail.com>
2026-01-26 17:51:25 +0100

    Remove GitHub actions (#413)

--- src/ingestion/main.py
+++ src/ingestion/main.py
@@ -1,17 +1,8 @@
-import os
-
-from src.ingestion.github_actions import GitHubActionsExtractor
 from src.ingestion.github_repo_interactors import main as github_repo_interactors_run
 from src.ingestion.medium_web_scraper import MediumWebScraper
 
 
 def main() -> None:
-    default_lookback = 7
-    try:
-        lookback_days = int(os.getenv("INGESTION_LOOKBACK_WINDOW", default_lookback))
-    except:
-        lookback_days = default_lookback
-
     tags = [
         "analytics-engineering",
         "apache-airflow",
@@ -19,7 +10,6 @@ def main() -> None:
         "dbt",
     ]
 
-    GitHubActionsExtractor(lookback_days=lookback_days).run()
     github_repo_interactors_run()
     MediumWebScraper(
         tags=tags,
